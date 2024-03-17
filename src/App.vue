34f450e6777bf96519153fe9799ab84d01292e8b
hasteds <tin.petr@gmail.com>
2024-03-17 14:58:56 +0100

    17.03

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template>
   <h1>{{  title }}</h1>>
-  <input type="text" ref="name">
-  <button @click="handleClick">Click me</button>
-  <Modal />
+  <p>Welcome, click bellow to open modal</p>
+
+  <div v-if="showModal">
+    <Modal theme="sale" @close="openModal">
+      <template v-slot:links>
+        <a href="https:/www.bug.hr">Bug.hr</a>
+        <a href="https:/www.facebook.com">Facebook.com</a>
+      </template>
+      <h1>Modal 01</h1>
+      <p>This is modal text area</p>
+    </Modal>
+  </div>
+
+  <div v-if="showModal2">
+    <Modal theme="normal" @close="openModal2">
+      <template v-slot:sometext>
+        <h5>Modal 2 </h5>
+        <p>This is text are of second modal</p>
+      </template>
+    </Modal>
+  </div>
+
+  <br>
+  <br>
+  <button @click="openModal">Open Modal</button>
+  <button @click="openModal2">Open alternative modal</button>
+  
 </template>
 
 <script>
@@ -15,14 +39,19 @@ export default {
   },
   data() {
     return {
-      title: 'My first Vue application'
+      title: 'My first Vue application',
+      header: 'Sign up for the giveaway',
+      text: 'Oh my gawd what is this long ass text doing here',
+      showModal: false,
+      showModal2: false
     }
   },
   methods: {
-    handleClick() {
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
+    openModal() {
+      this.showModal = !this.showModal
+    },
+    openModal2() {
+      this.showModal2 = !this.showModal2
     }
   }
 }
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,15 +1,30 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
-            <h1>Modal title</h1>
-            <p>modal content</p>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale'}">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+                <slot name="sometext"></slot>
+            </div>
+            <button @click="closeModal">Close Modal</button>
         </div>
     </div>
 </template>
 
+<script>
+    export default {
+        props: ['theme'],
+        methods: {
+            closeModal() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
 <style>
     .modal {
-        width: 400px;
+        width: 600px;
         padding: 20px;
         margin: 100px auto;
         background: white;
@@ -26,5 +41,24 @@
         color: rgb(14, 212, 186);
         border: none;
         padding: 0;
+    }
+    .modal.sale {
+        background: crimson;
+        color: white;
+    }
+    .modal.sale.h1 {
+        color: white;
+    }
+    .actions {
+        text-align: center;
+        margin: 30px 0 10px 0;
+    }
+    .actions a {
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
     }
 </style>
